99e698beb9610977b6bdac71c14a4333fc3b5066
gustavo martins <gustavomartinsdp@outlook.com>
2020-06-07 21:25:41 -0300

    Updates

---
+++ precache-manifest.8b8817b842dc0dc4ee96758c63de5f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e758f6af39eccb2a1d0adfd74a6e1467",
+    "url": "/pricing-component-with-toggle-button/index.html"
+  },
+  {
+    "revision": "c0039958e624296ca388",
+    "url": "/pricing-component-with-toggle-button/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5f145e686c5e97652953",
+    "url": "/pricing-component-with-toggle-button/static/css/main.469f0a51.chunk.css"
+  },
+  {
+    "revision": "c0039958e624296ca388",
+    "url": "/pricing-component-with-toggle-button/static/js/2.9430a901.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pricing-component-with-toggle-button/static/js/2.9430a901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f145e686c5e97652953",
+    "url": "/pricing-component-with-toggle-button/static/js/main.4a46a684.chunk.js"
+  },
+  {
+    "revision": "feb8ce28b83d0a4bcbe6",
+    "url": "/pricing-component-with-toggle-button/static/js/runtime-main.97d8ebc9.js"
+  }
+]);
